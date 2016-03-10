fb9b6db47dd0ac5777024e37c8cd32a5597d3852
Jakkawad <admin@all2sale-imac.local>
2016-03-10 13:55:51 +0700

    Initial Commit

---
+++ CollectionViewE2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewE2
+//
+//  Created by admin on 3/10/2559 BE.
+//  Copyright © 2559 All2Sale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
